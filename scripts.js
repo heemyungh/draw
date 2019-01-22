1efde14217fe5ad4bb162c83d4db8a5a4d739d3d
Heemyung H <hhwang201501@college.harvard.edu>
2019-01-21 20:25:40 -0500

    implement undo functionality

--- scripts.js
+++ scripts.js
@@ -4,14 +4,31 @@ if (!localStorage.getItem('canvas')) {
     localStorage.setItem('canvas', "");
 }
 
+// current stroke being drawn
+let stroke = [];
+
 document.addEventListener("DOMContentLoaded", function() {
     document.querySelector("#svg").innerHTML = localStorage.getItem("canvas");
+    document.querySelector("#undo").onclick = undo;
     document.querySelector("#clear").onclick = clear;
     setInterval(save, 60000);
+    document.onkeyup = function(e) {
+        if (e.ctrlKey && e.which == 90) {
+            undo();
+        }
+    };
 });
 
 function save() {
     localStorage.setItem("canvas", document.querySelector("#svg").innerHTML);
+}
+
+
+function undo() {
+    for (let i = stroke.length; i > 0; i--) {
+        stroke[i-1].remove();
+        stroke.pop();
+    }
 }
 
 function clear() {
